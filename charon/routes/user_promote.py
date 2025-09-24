a5cc9f6d01bfb67739a576f6af027c82490a7f34
Vic <vicxd@pm.me>
2025-09-24 21:41:35 +0200

    Add warn on error in PB dispatch

--- charon/routes/user_promote.py
+++ charon/routes/user_promote.py
@@ -153,7 +153,7 @@ async def promote_user(data: UserPromoteRequest, program: Program) -> JSONRespon
 
         # Dispatch the upgrade to Professor Bloom
         if config.bloom_token:
-            env.http.post(
+            async with env.http.post(
                 f"https://professorbloom.hackclub.com/webhook/charon/{config.bloom_token}",
                 data=json.dumps({
                     "user_id": data.id,
@@ -162,7 +162,18 @@ async def promote_user(data: UserPromoteRequest, program: Program) -> JSONRespon
                     "action": "promotion"
                 }),
                 headers={"Content-Type": "application/json"}
-            )
+            ) as response:
+                if response.ok == False:
+                    text = await response.text()
+                    logger.warning(
+                        f"Failed to dispatch promotion of {data.id} to Professor Bloom: {text}"
+                    )
+                    await send_heartbeat(
+                        f":neodog_sad: Failed to dispatch promotion of `{data.id}` to Professor Bloom: `{text}`",
+                        [f"```{response_json}```"],
+                        production=True,
+                    )
+                    
 
         return JSONResponse(
             status_code=200 if ok else 422,
